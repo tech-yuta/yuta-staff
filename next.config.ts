a8bf562c0086d799bb18e701908282becdaea358
Tam Phan <phanminhtam1992@gmail.com>
2026-04-06 01:06:23 +0200

    fix: remove skipWaiting option from PWA configuration

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,6 @@ export default withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
